26e7f811a92154f8a247bfa1534bd4d7c766ad91
Tim Nelson <traveltimn@gmail.com>
2020-10-26 12:33:04 +0000

    Add category filtering

--- products/views.py
+++ products/views.py
@@ -8,20 +8,32 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    categories = None
     query = None
 
     if request.GET:
+
+        if "category" in request.GET:
+            categories = request.GET["category"].split(",")
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if "q" in request.GET:
             query = request.GET["q"]
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(
+                    request, "You didn't enter any search criteria!")
                 return redirect(reverse("products"))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = (
+                Q(name__icontains=query) |
+                Q(description__icontains=query)
+            )
             products = products.filter(queries)
 
     context = {
         "products": products,
+        "current_categories": categories,
         "search_term": query,
     }
 
